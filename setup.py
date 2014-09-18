f49fe65d5bff2202ed604a02949966d9270b85a1
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-09-18 09:33:02 +0200

    jsonrpclib is now a mandatory requirement

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 
 # Module version
-__version_info__ = (0, 2, 0)
+__version_info__ = (0, 2, 1)
 __version__ = ".".join(str(x) for x in __version_info__)
 
 # Documentation strings format
